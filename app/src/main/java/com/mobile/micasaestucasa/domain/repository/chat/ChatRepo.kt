7cb2a76da874b703316a9ab8e082dba01601e1bd
bmm333 <benmema3@gmail.com>
2026-04-16 21:00:30 +0200

    feat(chat): Chat Domain first impl

---
+++ app/src/main/java/com/mobile/micasaestucasa/domain/repository/chat/ChatRepo.kt
@@ -0,0 +1,37 @@
+package com.mobile.micasaestucasa.domain.repository.chat
+
+import com.mobile.micasaestucasa.domain.model.chat.Conversation
+import com.mobile.micasaestucasa.domain.model.chat.Message
+
+/**
+ * Real time chat operations Contract
+ *
+ * */
+interface ChatRepo {
+
+    /**
+     *  sends a message which can be textual or image,
+     *  if the message is an image then this method should only be called after the image is uploaded to firebase
+     *  repo dose not handle image upload
+     *
+     * */
+    suspend fun sendMessage(conversationId:String,
+                            senderId:String,
+                            text:String,
+                            imageUrl:String?=null): Result<Message>
+
+    suspend fun getOrCreateConversation(hostId:String, renterId:String, propertyId:String): Result<String>
+
+    /**
+     * this function marks all not read messages as read for the current user
+     *
+     * */
+    suspend fun markMessagesAsRead(
+        conversationId: String,
+        userId: String
+    ): Result<Unit>
+    /**
+     * Returns all the conversations of an unser ordered by last message timestamp
+     * */
+    suspend fun getConversationsForUser(userId:String):Result<List<Conversation>>
+}

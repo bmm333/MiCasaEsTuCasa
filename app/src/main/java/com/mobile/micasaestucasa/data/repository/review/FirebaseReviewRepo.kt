18be8f7183f57c65be72d289f45e99a43a83edbb
bmm333 <benmema3@gmail.com>
2026-04-26 16:50:17 +0200

    feat(review):DI for review repo

--- app/src/main/java/com/mobile/micasaestucasa/data/repository/review/FirebaseReviewRepo.kt
+++ app/src/main/java/com/mobile/micasaestucasa/data/repository/review/FirebaseReviewRepo.kt
@@ -1,4 +1,49 @@
 package com.mobile.micasaestucasa.data.repository.review
 
-class FirebaseReviewRepo {
+import com.google.firebase.firestore.FirebaseFirestore
+import com.mobile.micasaestucasa.domain.model.review.Review
+import com.mobile.micasaestucasa.domain.repository.review.ReviewRepo
+import javax.inject.Inject
+
+class FirebaseReviewRepo @Inject constructor(private val firestore: FirebaseFirestore): ReviewRepo {
+
+    private val collection=firestore.collection("reviews")
+    override suspend fun WriteReview(
+        review: Review,
+        userId: String
+    ): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getPropertyReviews(propertyId: String): Result<List<Review>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUserReviews(userId: String): Result<List<Review>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteReview(reviewId: String): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun editReview(reviewId: String): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun replyToReview(
+        reviewId: String,
+        reply: String
+    ): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun hasUserAlreadyReviewed(
+        userId: String,
+        propertyId: String
+    ): Result<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+
 }

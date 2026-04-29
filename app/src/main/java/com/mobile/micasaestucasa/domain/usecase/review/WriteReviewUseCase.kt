59b3dba9eff3d6371e6877f9256d6f440f2f01c9
bmm333 <benmema3@gmail.com>
2026-04-29 19:32:12 +0200

    feat(review):Update WriteReviewUseCase

--- app/src/main/java/com/mobile/micasaestucasa/domain/usecase/review/WriteReviewUseCase.kt
+++ app/src/main/java/com/mobile/micasaestucasa/domain/usecase/review/WriteReviewUseCase.kt
@@ -1,10 +1,14 @@
 package com.mobile.micasaestucasa.domain.usecase.review
 
+import com.mobile.micasaestucasa.domain.model.booking.BookingStatus
 import com.mobile.micasaestucasa.domain.model.review.Review
+import com.mobile.micasaestucasa.domain.model.review.ReviewType
+import com.mobile.micasaestucasa.domain.repository.booking.BookingRepo
 import com.mobile.micasaestucasa.domain.repository.review.ReviewRepo
 import javax.inject.Inject
 
-class WriteReviewUseCase @Inject constructor(private val reviewRepo: ReviewRepo) {
+class WriteReviewUseCase @Inject constructor(private val reviewRepo: ReviewRepo,private
+val bookingRepo: BookingRepo) {
     suspend operator fun invoke(review: Review,userId: String): Result<Unit> {
         if(userId.isEmpty())
             return Result.failure(IllegalArgumentException("Id utente non puo essere vuoto"))
@@ -14,6 +18,33 @@ class WriteReviewUseCase @Inject constructor(private val reviewRepo: ReviewRepo)
             return Result.failure(IllegalArgumentException("Descrizione obbligatoria"))
         if (review.stars < 1 || review.stars > 5)
             return Result.failure(IllegalArgumentException("Valutazione obbligatoria compresa tra 1 e 5"))
+        val bookingResult=bookingRepo.getBookingById(review.bookingId)
+        if(bookingResult.isFailure)
+            return Result.failure(
+                IllegalStateException("Booking not found")
+            )
+        val booking=bookingResult.getOrThrow()
+        if(booking.status!= BookingStatus.COMPLETED)
+            return Result.failure(IllegalStateException("You can review the property once the stay is over"))
+        when(review.reviewType)
+        {
+            ReviewType.PROPERTY_REVIEW->{
+            if (booking.renterId != userId)
+                return Result.failure(IllegalStateException("Only the renter can leave a review in the property"))
+            if (review.targetId != booking.propertyId)
+                return Result.failure(IllegalStateException("Dose not match the property"))
+
+            }
+            ReviewType.RENTER_REVIEW->{
+                if(booking.hostId!=userId)
+                    return Result.failure(IllegalStateException("Only the host can review the renter"))
+                if(review.targetId!=booking.renterId)
+                    return Result.failure(IllegalStateException("Dose not match the renter"))
+            }
+        }
+        val alreadyReviewd=reviewRepo.hasUserAlreadyReviewedBooking(userId=userId,bookingId = review.bookingId, type = review.reviewType)
+        if(alreadyReviewd.getOrDefault(false))
+                return Result.failure(IllegalStateException("You've already given a review ${review.reviewType.name} per questo soggiorno"))
         return reviewRepo.writeReview(review,userId)
     }
 }
